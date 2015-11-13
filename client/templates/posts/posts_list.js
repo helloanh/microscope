d1a551ecf3c61743eb26b554a3b239dda7bc7dbc
skatermonk88 <akhoang88@gmail.com>
2015-11-13 16:13:12 +0700

    rm autopublish and set up basic publication

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,20 +1,20 @@
-var postsData = [ {
-    title: 'Introducing Telescope',
-    url: 'http://sachagreif.com/introducing-telescope/'
-  },
-  {
-    title: 'Meteor',
-    url: 'http://meteor.com'
-}, {
-    title: 'The Meteor Book',
-    url: 'http://themeteorbook.com'
-}, {
+// var postsData = [ {
+//     title: 'Introducing Telescope',
+//     url: 'http://sachagreif.com/introducing-telescope/'
+//   },
+//   {
+//     title: 'Meteor',
+//     url: 'http://meteor.com'
+// }, {
+//     title: 'The Meteor Book',
+//     url: 'http://themeteorbook.com'
+// }, {
 
-    title: 'Yo This is My Post',
-    url: 'randomexampleurl.com'
-}
+//     title: 'Yo This is My Post',
+//     url: 'randomexampleurl.com'
+// }
 
-];
+// ];
 
 
 Template.postsList.helpers({
