c21ad2e62dd03dbcb01af8297e4522a760fe1607
skatermonk88 <akhoang88@gmail.com>
2015-11-13 16:38:10 +0700

    setup basic routes

--- client/templates/posts/posts_list.js
+++ client/templates/posts/posts_list.js
@@ -1,22 +1,3 @@
-// var postsData = [ {
-//     title: 'Introducing Telescope',
-//     url: 'http://sachagreif.com/introducing-telescope/'
-//   },
-//   {
-//     title: 'Meteor',
-//     url: 'http://meteor.com'
-// }, {
-//     title: 'The Meteor Book',
-//     url: 'http://themeteorbook.com'
-// }, {
-
-//     title: 'Yo This is My Post',
-//     url: 'randomexampleurl.com'
-// }
-
-// ];
-
-
 Template.postsList.helpers({
   posts: function() {
     return Posts.find();
